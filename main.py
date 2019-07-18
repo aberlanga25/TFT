ebeeeb428007bcd224a9ce01e26466a05e61cdee
aberlanga25 <natacion3>
2019-07-18 12:57:08 -0600

    work

--- main.py
+++ main.py
@@ -1,62 +1,79 @@
-
-import os
-
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 
-
-from kivymd.list import BaseListItem
-from kivymd.material_resources import DEVICE_TYPE
-from kivymd.navigationdrawer import MDNavigationDrawer, NavigationDrawerHeaderBase
 from kivymd.theming import ThemeManager
-from kivymd.list import ILeftBody
-
-# User defined imports
-from kivy.core.window import Window
-
-# Window.fullscreen = "auto"
+from kivymd.navigationdrawer import NavigationLayout
+from kivymd.list import OneLineAvatarListItem, ILeftBody
 
 
-class MainApp(App):
+
+class MyLayout(BoxLayout):
+    scr_mngr = ObjectProperty(None)
+    def change_screen(self, screen, *args):
+        self.scr_mngr.transition.direction = 'left'
+        self.scr_mngr.current = screen
+class nav_layout(NavigationLayout):
+    def print_text(self):
+        print('hello')
+
+    def change_screen(self, screen, *args):
+        self.ids.scr_mngr.transition.direction = 'left'
+        self.ids.scr_mngr.current = screen
+
+
+class MDCustomIconItem(OneLineAvatarListItem):
+    icon = StringProperty('')
+    text = StringProperty()
+
+    def _set_active(self, active, list):
+        pass
+class AvatarSampleWidget(ILeftBody, Image):
+    pass
+
+
+class MyApp(App):
     theme_cls = ThemeManager()
+    theme_cls.primary_palette = 'BlueGrey'
+    theme_cls.theme_style = 'Dark'
     title = "TFT"
     main_widget = None
 
     def build(self):
-        self.main_widget = Builder.load_file(
-            os.path.join(os.path.dirname(__file__), "./main.kv")
-        )
-        self.theme_cls.theme_style = 'Dark'
-
-#        self.main_widget.ids.text_field_error.bind(
-#            on_text_validate=self.set_error_message,
-#            on_focus=self.set_error_message)
-        self.bottom_navigation_remove_mobile(self.main_widget)
+        self.main_widget = Builder.load_file('main.kv')
         return self.main_widget
 
+    def callback(self, instance, value):
+        print("Pressed item menu %d" % value)
+
+    def callbackItems(self):
+        self.main_widget.ids.scr_mngr.transition.direction = 'right'
+        self.main_widget.ids.scr_mngr.current = 'items'
+    def callbackChamps(self):
+        self.main_widget.ids.scr_mngr.transition.direction = 'right'
+        self.main_widget.ids.scr_mngr.current = 'champs'
+    def callbackTier(self):
+        self.main_widget.ids.scr_mngr.transition.direction = 'right'
+        self.main_widget.ids.scr_mngr.current = 'tier'
+
+    def on_start(self):
+        self.main_widget.ids.nav_drawer.add_widget(
+            MDCustomIconItem(
+                text="Items",
+                icon='images/menu/items.png',
+                on_release=lambda x: self.callbackItems()))
+        self.main_widget.ids.nav_drawer.add_widget(
+            MDCustomIconItem(
+                text="Champions",
+                icon='images/menu/champs.png',
+                on_release=lambda x: self.callbackChamps()))
+        self.main_widget.ids.nav_drawer.add_widget(
+            MDCustomIconItem(
+                text="Tier List",
+                icon='images/menu/tier.png',
+                on_release=lambda x: self.callbackTier()))
 
 
-    def bottom_navigation_remove_mobile(self, widget):
-        # Removes some items from bottom-navigation demo when on mobile
-        if DEVICE_TYPE == 'mobile':
-            widget.ids.bottom_navigation_demo.remove_widget(widget.ids.bottom_navigation_desktop_2)
-        if DEVICE_TYPE == 'mobile' or DEVICE_TYPE == 'tablet':
-            widget.ids.bottom_navigation_demo.remove_widget(widget.ids.bottom_navigation_desktop_1)
-
-    def set_error_message(self, *args):
-        if len(self.root.ids.text_field_error.text) == 2:
-            self.root.ids.text_field_error.error = True
-        else:
-            self.root.ids.text_field_error.error = False
-
-    def on_pause(self):
-        return True
-
-    def on_stop(self):
-        pass
-
-
-if __name__ == '__main__':
-    MainApp().run()
+MyApp().run()
