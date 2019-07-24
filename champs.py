887da45078017b12f7065a3080aa9c381fa6860d
aberlanga25 <berlanga2512@gmail.com>
2019-07-23 22:06:51 -0600

    Home

--- champs.py
+++ champs.py
@@ -6,9 +6,9 @@ def listCosts():
 
 def listOrigins():
     origins = ['Demon', 'Wild', 'Ninja', 'Glacial','Glacial', 'Dragon', 'Robot', 'Demon','Glacial', 'Void', 'Imperial', 'Imperial',
-               'Demon', 'Demon', 'Noble','Pirate','Noble', 'Wild, Yordle', 'Pirate', 'Phantom','Void','Imperial','Noble',
+               'Demon', 'Demon', 'Noble','Pirate','Noble', 'Wild , Yordle', 'Pirate', 'Phantom','Void','Imperial','Noble',
                'Ninja, Yordle', 'Void', 'Phantom', 'Noble', 'Glacial', 'Noble', 'Yordle', 'Pirate', 'Phantom', 'Demon',
-               'Wild', 'Yordle', 'Pirate', 'Void', 'Wild', 'Glacial', 'Ninja', 'Dragon', 'Imperial, Demon', 'Yordle',
+               'Wild', 'Yordle', 'Pirate', 'Void', 'Wild', 'Glacial', 'Ninja', 'Dragon', 'Imperial , Demon', 'Yordle',
                'Pirate', 'Demon', 'Noble', 'Yordle', 'Glacial', 'Wild', 'Exile', 'Ninja']
     return origins
 
@@ -20,3 +20,28 @@ def listNames():
              'Shyvana', 'Swain', 'Tristana', 'Twisted Fate', 'Varus', 'Vayne', 'Veigar', 'Volibear', 'Warwick', 'Yasuo',
              'Zed']
     return names
+
+def listClasses():
+    classes = ['Blademaster','Sorcerer','Assassin', 'Elementalist', 'Ranger', 'Sorcerer', 'Brawler', 'Elementalist', 'Guardian',
+               'Brawler','Knight', 'Blademaster', 'Shapeshifter', 'Assassin', 'Blademaster', 'Gunslinger , Blademaster', 'Knight',
+               'Shapeshifter', 'Gunslinger','Sorcerer', 'Sorcerer', 'Assassin', 'Knight', 'Elementalist', 'Assassin', 'Ranger',
+               'Guardian', 'Elementalist', 'Gunslinger', 'Sorcerer', 'Gunslinger', 'Knight', 'Sorcerer', 'Shapeshifter', 'Knight',
+               'Assassin', 'Brawler', 'Assassin', 'Knight', 'Blademaster', 'Shapeshifter', 'Shapeshifter', 'Gunslinger',
+               'Sorcerer', 'Ranger', 'Ranger', 'Sorcerer', 'Brawler', 'Brawler', 'Blademaster', 'Assassin']
+    return classes
+
+def tier1():
+    tier1 = [5,10,13,17,18,39]
+    return tier1
+def tier2():
+    tier2 = [4,6,8,12,22,23,26,36,41,44,48]
+    return tier2
+def tier3():
+    tier3 = [2,3,7,9,20,24,25,27,28,29,30,31,33,34,42,45,46,47,49,50,51]
+    return tier3
+def tier4():
+    tier4 = [1,11,19,38,40]
+    return tier4
+def tier5():
+    tier5 = [14,15,16,21,32,35,37,43]
+    return tier5
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import  StringProperty
 from kivy.uix.image import Image
+from kivy.uix.gridlayout import GridLayout
+from kivy.metrics import dp
 
 from kivymd.theming import ThemeManager
 from kivymd.list import OneLineAvatarListItem, ILeftBody
@@ -11,8 +13,8 @@ from kivymd.utils.cropimage import crop_image
 from kivymd.label import MDLabel
 from kivymd.imagelists import SmartTile
 
-from champs import (listCosts, listOrigins, listNames)
-from demo_apps.basedialog import BaseDialogForDemo
+from champs import *
+from basedialog import BaseDialogForDemo
 
 class MDCustomIconItem(OneLineAvatarListItem):
     icon = StringProperty('')
@@ -53,24 +55,49 @@ class MyApp(App):
             crop_image(size, path_to_origin_image, path_to_crop_image)
         instance.source = path_to_crop_image
 
+    def tierChamp(self, x):
+        names = listNames()
+        champLay = GridLayout(rows=2,)
+        champLay.add_widget(MySmartTile(source='images/champs/%d.png' % x, size_hint_y=None))
+        champLay.add_widget(
+            MDLabel(text=str(names[x - 1]), size_hint_y=None, font_style="Subtitle2", theme_text_color='Primary',
+                    halign='center', valign='top', size=(dp(10),dp(10))))
+        return champLay
+    def set_tiers(self):
+        t1 = tier1()
+        t2 = tier2()
+        t3 = tier3()
+        t4 = tier4()
+        t5 = tier5()
+
+        for x in t1:
+            self.main_widget.ids.tier1.add_widget(self.tierChamp(x))
+        for y in t2:
+            self.main_widget.ids.tier2.add_widget(self.tierChamp(y))
+        for z in t3:
+            self.main_widget.ids.tier3.add_widget(self.tierChamp(z))
+        for w in t4:
+            self.main_widget.ids.tier4.add_widget(self.tierChamp(w))
+        for v in t5:
+            self.main_widget.ids.tier5.add_widget(self.tierChamp(v))
+
 
     def set_champs(self):
         costs = listCosts()
         orig = listOrigins()
         names = listNames()
-
+        clas = listClasses()
         for x in range(1,52):
-            self.main_widget.ids.champsgrid.add_widget(
-                MySmartTile(source='images/champs/%d.png' %x,  size_hint_y=None, size_hint_x=0.3)
-            )
-            self.main_widget.ids.champsgrid.add_widget(
-                MDLabel(text=str(names[x-1]), size_hint_y=None, font_style="Subtitle2", theme_text_color='Primary'))
+            champLay = GridLayout(cols=2)
+            champLay.add_widget(MySmartTile(source='images/champs/%d.png' %x,  size_hint_y=None))
+            champLay.add_widget(MDLabel(text=str(names[x-1]), size_hint_y=None, font_style="Subtitle2", theme_text_color='Primary', halign='center'))
+            self.main_widget.ids.champsgrid.add_widget(champLay)
             self.main_widget.ids.champsgrid.add_widget(
                 MDLabel(text=str(costs[x-1]), size_hint_y=None, font_style="Subtitle2",halign='center', theme_text_color='Primary'))
             self.main_widget.ids.champsgrid.add_widget(
-                MDLabel(text=str(orig[x-1]), size_hint_y=None,  font_style="Subtitle2", theme_text_color='Primary'))
+                MDLabel(text=str(orig[x-1]), size_hint_y=None,  font_style="Subtitle2", halign='center', theme_text_color='Primary'))
             self.main_widget.ids.champsgrid.add_widget(
-                MDLabel(text="%d" % x, size_hint_y=None, font_style="Subtitle2", theme_text_color='Primary'))
+                MDLabel(text=str(clas[x-1]), size_hint_y=None, font_style="Subtitle2", halign='center', theme_text_color='Primary'))
 
     def createSmart(self):
         smart = MDLabel(text="hi", font_style="H6")
@@ -93,6 +120,6 @@ class MyApp(App):
                 icon='images/menu/tier.png',
                 on_release=lambda x: self.callbackMenu('tier')))
         self.set_champs()
-        #self.set_items()
+        self.set_tiers()
 
 MyApp().run()
